5baf24791a374b5e20e88f5f758b1be17b1581ed
filipnaumovic <filip@salsitasoft.com>
2015-03-01 22:43:41 +0100

    initial

---
+++ README.md
@@ -0,0 +1,27 @@
+# jQuery.viewable  
+  
+`$('#target').viewable()`  
+  
+**Returns a boolean value and only for the first element found in query! NOT chainable.**  
+  
+A quick library that checks for various attributes of when an element is visible to the user:  
+
+- `display: none` on target and/or parents (stops at `body`)
+- `visibility: hidden` on target and/or parents
+- `opacity` of target and/or parents (configurable minimum)
+- whether an element is in viewport (configurable tolerance)
+- **TODO** whether an element is below page fold (reachable by scrolling)
+- **TODO** whether an element is overlayed by a higher z-Index element
+- **TODO** whether an element is hidden by a combination of 3D transforms
+
+## Options
+`$('#target').viewable({  
+  opacityMin: 0.2,  
+  checkViewport: false
+});`  
+
+`checkOpacity` - Controls the opacity check, default `true`.  
+`opacityMin` - Minimum opacity value for a visible element, default `0`.    
+`checkParents` - Check CSS values of parent elements (stops at `body`), default `true`.  
+`checkViewport` - Checks whether a part of the element is in the visible viewport, default `true`.  
+`viewportTolerance` - Tolerance for the viewport check in pixels, default `0`.
---
+++ src/jquery.viewable.js
@@ -0,0 +1,84 @@
+/*!
+ * jquery-viewable
+ * filip@salsitasoft.com
+ * Released under the MIT license
+ */
+
+;(function($) {
+
+  // Defaults
+  var element = null;
+  var settings = null;
+  var debugEnabled = false;
+  var defaultOptions = {
+    checkOpacity: true,
+    opacityMin: 0,
+    checkParents: true,
+    checkViewport: true,
+    viewportTolerance: 0
+  };
+
+  // Is element hidden by CSS values?
+  var isVisible = function(el, opacity, opacityMin) {
+    // Expect opaque element if we're not checking it
+    var opaque = true;
+    var displayed = el.css('display') !== 'none' ? true : false;
+    var visible = el.css('visibility') !== 'hidden' ? true : false;
+    if (opacity) {
+      opaque = el.css('opacity') > opacityMin ? true : false;
+    };
+    return (displayed && visible && opaque);
+  };
+
+  // Is element in visible viewport?
+  var isInViewport = function(el, tolerance) {
+    var rect = el.get(0).getBoundingClientRect();
+    // Using jQuery width()/height() since it factors in overlayed scrollbars
+    return !(rect.bottom < 0 + tolerance ||
+             rect.right < 0 + tolerance ||
+             rect.left > $(window).width() - tolerance ||
+             rect.top > $(window).height() - tolerance);
+  };
+
+  // Create jQuery plugin
+  $.fn.viewable = function(options) {
+
+    // Get user settings
+    settings = $.extend({}, defaultOptions, options);
+
+    // Reference only first element found by query
+    element = $(this.get(0));
+
+    // Element CSS value check
+    if (!isVisible(element, settings.checkOpacity, settings.opacityMin)) {
+      // console.info('Element is not visible');
+      return false;
+    };
+
+    // Parents CSS value check
+    if (settings.checkParents) {
+      var state = true;
+      element.parents(':not(body, html)').each(function() {
+        if (!isVisible($(this), settings.checkOpacity, settings.opacityMin)) {
+          // console.info('Parent element is not visible');
+          state = false;
+          return false;
+        };
+      });
+      if (!state) return false;
+    };
+
+    // Viewport check
+    if (settings.checkViewport && !isInViewport(element, settings.viewportTolerance)) {
+      // console.info('Element is visible, but not in viewport (tolerance: ' + settings.viewportTolerance + 'px)');
+      return false;
+    };
+
+    // TODO: Below fold check
+    // TODO: z-index check
+    // TODO: debug
+
+    // Element is viewable
+    return true;
+  };
+})(jQuery, window, document);
